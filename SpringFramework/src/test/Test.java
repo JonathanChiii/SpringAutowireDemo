4767086b60a223a1a5ae3c48914e4b181a76fba1
Jonathan PC <chiqingyuan@hotmail.com>
2022-10-11 16:01:02 -0400

    Fisrt Commit.

--- SpringFramework/src/test/Test.java
+++ SpringFramework/src/test/Test.java
@@ -8,6 +8,8 @@ import business.Student;
 public class Test {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("resources/spring.xml");
+		// Constructor will be called for all beans in the spring.xml
+		
 		Student obj = (Student) ac.getBean("obj");
 		System.out.println(obj.toString());
 	}
